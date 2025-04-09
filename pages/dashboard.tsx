1050648b56245e4fdf8e8b41df81cde2dfbbcb01
eDiscoveryQC <ManriquezJoeA@gmail.com>
2025-04-09 16:57:51 -0600

    Delete pages/dashboard.tsx

--- pages/dashboard.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Dashboard() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Dashboard Page (Placeholder)</h1>
-    </main>
-  );
-}
