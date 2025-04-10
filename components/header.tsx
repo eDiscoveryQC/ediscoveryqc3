5272b3fe4b77dc47e971b92c512527b1212a1524
eDiscoveryQC <ManriquezJoeA@gmail.com>
2025-04-10 17:38:21 -0600

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,1 +1,42 @@
+// components/Header.tsx
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full bg-white shadow-lg z-50">
+      <div className="max-w-7xl mx-auto p-6 flex justify-between items-center">
+        {/* Logo in the header */}
+        <a href="#" className="flex items-center">
+          <img 
+            src="/ediscoveryqclogo.svg" 
+            alt="eDiscoveryQC Logo" 
+            className="h-20"
+          />
+        </a>
+        <nav className="space-x-6">
+          <Link href="/" passHref>
+            <a className="text-blue-700 hover:text-blue-900 transition">Home</a>
+          </Link>
+          <Link href="/qc-tools" passHref>
+            <a className="text-blue-700 hover:text-blue-900 transition">QC Tools</a>
+          </Link>
+          <Link href="#pricing" passHref>
+            <a className="text-blue-700 hover:text-blue-900 transition">Pricing</a>
+          </Link>
+          <Link href="#contact" passHref>
+            <a className="text-blue-700 hover:text-blue-900 transition">Contact</a>
+          </Link>
+          <Link href="/signup" passHref>
+            <a className="bg-blue-700 hover:bg-blue-800 text-white px-6 py-3 rounded-lg font-semibold transition">Start Free Trial</a>
+          </Link>
+          <Link href="/login" passHref>
+            <a className="text-blue-700 font-medium hover:underline transition-all duration-200">Sign In</a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
 
